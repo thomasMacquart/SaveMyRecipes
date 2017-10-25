f98d543ba4eb1de75d754d9fb0a92845de0239be
Thomas Macquart <thomas.macquart@icemobile.com>
2017-10-25 21:40:12 +0200

    first commit

---
+++ app/src/main/java/saverecipes/thomasmacquart/com/recipeme/RecipesListActivity.kt
@@ -0,0 +1,13 @@
+package saverecipes.thomasmacquart.com.recipeme
+
+import android.app.Activity
+import android.os.Bundle
+
+class RecipesListActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recipes_list_activity)
+    }
+
+}
