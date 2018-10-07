7f222e672f5d2a8c928679e4060fc6b09f40d724
Thomas Macquart <thomas.macquart@Thomass-MacBook-Pro-2.local>
2018-10-07 22:02:47 +0200

    run in different thread

--- app/src/main/java/saverecipes/thomasmacquart/com/recipeme/recipes/ui/viewmodel/RecipeListViewModel.kt
+++ app/src/main/java/saverecipes/thomasmacquart/com/recipeme/recipes/ui/viewmodel/RecipeListViewModel.kt
@@ -2,6 +2,7 @@ package saverecipes.thomasmacquart.com.recipeme.recipes.ui.viewmodel
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
+import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.launch
 import saverecipes.thomasmacquart.com.recipeme.recipes.domain.Recipe
 import saverecipes.thomasmacquart.com.recipeme.recipes.domain.RecipeRepo
@@ -15,7 +16,7 @@ import javax.inject.Inject
 open class RecipeListViewModel @Inject constructor(val repo : RecipeRepo) : ViewModel() {
     public lateinit var recipes: LiveData<RecipesListModel>;
 
-    open fun loadRecipes()  = launch {
+    open fun loadRecipes()  = launch(CommonPool) {
         val result = repo.getRecipes()
         recipes = result
     }
