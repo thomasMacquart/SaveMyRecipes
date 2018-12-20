ae1f10170c0171a87aac4b9575b73a08b591d35d
Thomas Macquart <thomas.macquart@Thomass-MacBook-Pro-2.local>
2018-12-20 13:09:48 +0100

    add networkmodule

---
+++ app/src/main/java/saverecipes/thomasmacquart/com/recipeme/core/di/NetworkModule.kt
@@ -0,0 +1,35 @@
+package saverecipes.thomasmacquart.com.recipeme.core.di
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "https://example"
+
+@Module
+abstract class NetworkModule {
+
+    @Provides
+    fun providesRetrofit(okHttpClient: OkHttpClient) =
+            Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
+                    .build()
+
+
+    @Provides
+    fun providesOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+    }
+}
